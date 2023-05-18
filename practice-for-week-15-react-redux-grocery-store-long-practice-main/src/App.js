b4e942d2e38f8e44aa68b9305b361f0742cf60a6
Ethanjonm <ethanjonmercado@gmail.com>
2023-05-18 15:25:56 -0400

    useEffect in App

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/App.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/App.js
@@ -1,9 +1,18 @@
 import { useState } from 'react';
 import Cart from './components/Cart';
 import ProduceList from './components/ProduceList';
+import { useDispatch } from 'react-redux';
+import { populateProduce } from './store/produce';
+import { useEffect } from 'react';
 
 function App() {
   const [showCart, setShowCart] = useState(false);
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(populateProduce())
+  }, [dispatch])
   
   return (
     <>

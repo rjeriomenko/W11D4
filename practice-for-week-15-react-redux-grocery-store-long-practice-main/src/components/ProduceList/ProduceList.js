0f27ad23d3bc4afdb6b35b3194c8b043c9646127
Ethanjonm <ethanjonmercado@gmail.com>
2023-05-18 15:39:39 -0400

    phase 2

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/components/ProduceList/ProduceList.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/components/ProduceList/ProduceList.js
@@ -1,10 +1,13 @@
 import ProduceDetails from './ProduceDetails';
 import './ProduceList.css';
+import { useSelector } from 'react-redux';
 
 function ProduceList() {
-  const produce = {};
+  const produce = useSelector(state => state.produce)
+  // console.log(produce)
   
   const produceArr = Object.values(produce);
+  
 
   return (
     <>
